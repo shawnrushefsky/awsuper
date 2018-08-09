19a2f630f1d3f1981919acad471ed0629ea6863f
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-08-09 12:31:34 -0500

    more rabbit tests

--- server/tests/clients/rabbit-tests.js
+++ server/tests/clients/rabbit-tests.js
@@ -19,47 +19,95 @@ describe('Rabbit Client', () => {
         await rabbit.disconnect();
     });
 
-    it('provides a connect method', async () => {
-        expect(rabbit.connect).to.be.a('function');
-        expect(rabbit.publishChannel).to.be.undefined;
-        expect(rabbit.consumeChannel).to.be.undefined;
+    describe('connect', () => {
+        afterEach(async () => {
+            await rabbit.disconnect();
+        });
 
-        const amqpConnect = sandbox.spy(amqp, 'connect');
+        it('invokes amqp.connect with the configured URL', async () => {
+            const amqpConnect = sandbox.spy(amqp, 'connect');
 
-        await rabbit.connect();
+            await rabbit.connect();
 
-        const expectedURL = `amqp://${config.rabbit.host}:${config.rabbit.port}`;
-        expect(amqpConnect.calledOnceWith(expectedURL)).to.be.true;
+            const expectedURL = `amqp://${config.rabbit.host}:${config.rabbit.port}`;
+            expect(amqpConnect.calledOnceWith(expectedURL)).to.be.true;
+        });
 
-        // The channels will have been created in this method as well
-        expect(rabbit.publishChannel).to.be.an('object');
-        expect(rabbit.consumeChannel).to.be.an('object');
+        it('establishes publish and consume channels', async () => {
+            expect(rabbit.publishChannel).to.be.undefined;
+            expect(rabbit.consumeChannel).to.be.undefined;
+
+            await rabbit.connect();
+
+            // The channels will have been created in this method as well
+            expect(rabbit.publishChannel).to.be.an('object');
+            expect(rabbit.consumeChannel).to.be.an('object');
+        });
     });
 
-    it('provides a disconnect method', async () => {
-        expect(rabbit.disconnect).to.be.a('function');
+    describe('disconnect', () => {
+        beforeEach(async () => {
+            await rabbit.connect();
+        });
 
-        await rabbit.connect();
+        it('closes the publish and consume channels, and the connection', async () => {
+            const publishClose = sandbox.spy(rabbit.publishChannel, 'close');
+            const consumeClose = sandbox.spy(rabbit.consumeChannel, 'close');
+            const connectionClose = sandbox.spy(rabbit.connection, 'close');
 
-        expect(rabbit.publishChannel).to.be.an('object');
-        expect(rabbit.consumeChannel).to.be.an('object');
+            await rabbit.disconnect();
 
-        const publishClose = sandbox.spy(rabbit.publishChannel, 'close');
-        const consumeClose = sandbox.spy(rabbit.consumeChannel, 'close');
-        const connectionClose = sandbox.spy(rabbit.connection, 'close');
+            // It will close the channels, and the connection
+            expect(publishClose.called).to.be.true;
+            expect(consumeClose.called).to.be.true;
+            expect(connectionClose.called).to.be.true;
+        });
 
-        await rabbit.disconnect();
+        it('sets connected to false, and the connection and channels to undefined', async () => {
+            expect(rabbit.connected).to.be.true;
+            expect(rabbit.connection).to.be.an('object');
+            expect(rabbit.publishChannel).to.be.an('object');
+            expect(rabbit.consumeChannel).to.be.an('object');
 
-        // It will close the channels, and the connection
-        expect(publishClose.called).to.be.true;
-        expect(consumeClose.called).to.be.true;
-        expect(connectionClose.called).to.be.true;
+            await rabbit.disconnect();
 
-        // It will set connected to false, and remove the connection and channel objects
-        expect(rabbit.connected).to.be.false;
-        expect(rabbit.connection).to.be.undefined;
-        expect(rabbit.publishChannel).to.be.undefined;
-        expect(rabbit.consumeChannel).to.be.undefined;
+            // It will set connected to false, and remove the connection and channel objects
+            expect(rabbit.connected).to.be.false;
+            expect(rabbit.connection).to.be.undefined;
+            expect(rabbit.publishChannel).to.be.undefined;
+            expect(rabbit.consumeChannel).to.be.undefined;
+        });
     });
 
+    describe('maybeAssertQueue', async () => {
+        before(async () => {
+            await rabbit.connect();
+        });
+
+        after(async () => {
+            await rabbit.disconnect();
+        });
+
+        it('asserts a queue if this client has not previously asserted a queue by that name', async () => {
+            // This client has not previously asserted any queues
+            expect(Object.keys(rabbit.assertedQueues).length).to.equal(0);
+
+            const assertQueue = sandbox.spy(rabbit.publishChannel, 'assertQueue');
+
+            await rabbit.maybeAssertQueue('testQueue');
+
+            expect(assertQueue.called).to.be.true;
+            expect(rabbit.assertedQueues.testQueue).to.be.true;
+        });
+
+        it('does not assert a queue if this client has previously asserted a queue by the same name', async () => {
+            rabbit.assertedQueues.testQueue = true;
+
+            const assertQueue = sandbox.spy(rabbit.publishChannel, 'assertQueue');
+
+            await rabbit.maybeAssertQueue('testQueue');
+
+            expect(assertQueue.called).to.be.false;
+        });
+    });
 });
