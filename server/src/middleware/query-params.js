4f24cc375f0e038b78825bf60ad84d9ac3f51c6a
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-08-11 13:11:01 -0500

    start on query params middleware

---
+++ server/src/middleware/query-params.js
@@ -0,0 +1,151 @@
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const moment = require('moment');
+const { coerceValue, DATE_FORMATS } = require('../utils/coerce');
+const { Types } = mongoose.Schema;
+
+/**
+ * This will return a middleware that parses query parameters into a mongo find query
+ * that corresponds to the provided model. It handles type conversion, $or, and range queries.
+ * @param {mongoose.Model} model
+ */
+async function queryParamsMiddleware(model) {
+    return async (req, res, next) => {
+        const { query: rawQuery } = req.query;
+
+        let query = {};
+        let errors = [];
+
+        for (let key in rawQuery) {
+            let rawValue = rawQuery[key];
+            if (_.isString(rawValue)) {
+                let { value, error } = coerceValue(key, rawValue, model);
+
+                if (error) {
+                    errors.push(error);
+                    continue;
+                }
+
+                query[key] = value;
+            } else if (Array.isArray(rawValue)) {
+                /**
+                 * ?key=value1&key=value2 gets received as { key: [value1, value2] }
+                 *
+                 * We are going to interpret this as an $or query, where key can be either value1 or value2
+                 */
+                query.$or = rawValue.map(opt => {
+                    let { value, error } = coerceValue(key, rawValue, model);
+
+                    if (error) {
+                        errors.push(error);
+                    } else {
+                        return { [opt]: value };
+                    }
+                });
+            } else if (_.isPlainObject(rawValue)) {
+                /**
+                 * ?key[min]=45&key[max]=100 get received as { key { min: 45, max: 100 }}
+                 *
+                 * We use this to construct a range query
+                 */
+                const fieldType = model.schema.paths[key];
+
+                let queryFunc;
+
+                if (fieldType === Types.Date) {
+                    queryFunc = getDateRangeQuery;
+                } else {
+                    queryFunc = getRangeQuery;
+                }
+
+                let { value, error } = queryFunc(key, rawValue, model);
+
+                if (error) {
+                    errors.push(error);
+                } else {
+                    query[key] = value;
+                }
+            }
+        }
+
+        // If there were any errors processing the query params, return a 400
+        if (errors.length > 0) {
+            return res.status(400).json({ errors });
+        }
+
+        req.mongoQuery = query;
+        return next();
+    };
+}
+
+/**
+ * Construct a date range query, using moment.js to understand date strings
+ * @param {String} key
+ * @param {Object} rawValue
+ * @param {mongoose.Model} model unsused here, but required to match an interface.
+ */
+function getDateRangeQuery(key, rawValue, model) {
+    const { min, max } = rawValue;
+    let query = {};
+
+    if (min) {
+        let date = moment(min, DATE_FORMATS);
+
+        if (!date.isValid()) {
+            return { error: `${min} is not a valid date format. Use one of: ${DATE_FORMATS}` };
+        }
+
+        query.$gte = date;
+    }
+
+    if (max) {
+        let date = moment(max, DATE_FORMATS);
+
+        if (!date.isValid()) {
+            return { error: `${max} is not a valid date format. Use one of: ${DATE_FORMATS}` };
+        }
+
+        query.$lte = date;
+    }
+
+    return { value: query };
+}
+
+/**
+ * Construct a range query, applying appropriate type coercion
+ * @param {String} key
+ * @param {Object} rawValue
+ * @param {mongoose.Model} model This is used to handle type coercion correctly for the model
+ */
+function getRangeQuery(key, rawValue, model) {
+    const { min, max } = rawValue;
+    let query = {};
+
+    if (min) {
+        let { value, error } = coerceValue(key, min, model);
+
+        if (error) {
+            return { error };
+        }
+
+        query.$gte = value;
+    }
+
+    if (max) {
+        let { value, error } = coerceValue(key, max, model);
+
+        if (error) {
+            return { error };
+        }
+
+        query.$lte = value;
+    }
+
+    return { value: query };
+}
+
+module.exports = {
+    queryParamsMiddleware,
+    getDateRangeQuery,
+    getRangeQuery
+};
---
+++ server/src/utils/coerce.js
@@ -0,0 +1,133 @@
+const mongoose = require('mongoose');
+const { Types } = mongoose.Schema;
+const { ObjectId } = mongoose.Types;
+const moment = require('moment');
+
+// These are the formats we will be accepting dates for query parameters
+const DATE_FORMATS = [
+    'MM-DD-YYYY',
+    'MM-DD-YYYY HH',
+    'MM-DD-YYYY HH:mm',
+    'MM-DD-YYYY HH:mm:ss',
+    'YYYY-MM-DD HH:mm:ss'
+];
+
+/**
+ * Coerce value into the appropriate type for key, as determined by model
+ * @param {String} key
+ * @param {String} value
+ * @param {mongoose.Model} model
+ */
+function coerceValue(key, value, model) {
+    const fieldType = model.schema.paths[key];
+
+    if (!fieldType) {
+        return { error: `${key} is not a valid field.` };
+    }
+
+    if (fieldType instanceof Types.String || fieldType instanceof Types.Mixed) {
+        return { value: value };
+    }
+    else if (fieldType instanceof Types.Boolean) {
+        return coerceBoolean(key, value);
+    } else if (fieldType instanceof Types.Number) {
+        return coerceNumber(key, value);
+    } else if (fieldType instanceof Types.ObjectId) {
+        return coerceObjectId(key, value);
+    } else if (fieldType instanceof Types.Date) {
+        return coerceDate(key, value);
+    } else {
+        return { error: `Type coercion is not currently supported for ${fieldType.instance}.` };
+    }
+}
+
+function coerceBoolean(key, value) {
+    if (value === 'false') {
+        return { value: false };
+    } else if (value === 'true') {
+        return { value: true };
+    } else {
+        return { error: `Expected type:Boolean for field ${key}` };
+    }
+}
+
+function coerceNumber(key, value) {
+    if (isNaN(key)) {
+        return { error: `Expected type:Number for field ${key}` };
+    } else {
+        return { value: Number(value) };
+    }
+}
+
+function coerceObjectId(key, value) {
+    try {
+        return { value: ObjectId(value) };
+    } catch (e) {
+        return { error: `Expected type:ObjectId for field ${key}` };
+    }
+}
+
+/**
+ * Creates a date range query from a specific date. For instance, if a user asks for date_updated=2018-4-11,
+ * it will search for all records where the date_updated value falls somewhere on that day
+ * @param {String} key
+ * @param {String} value
+ */
+function coerceDate(key, value) {
+    let date = moment(value, DATE_FORMATS);
+
+    if (!date.isValid()) {
+        return { error: `Expected type:Datetime for field ${key}` };
+    }
+
+    let actualResolution;
+
+    /**
+     * If a user provided anything less than a full timestamp, we should assume they want
+     * all records from within the resolution they specified.
+     *
+     * Example 1: User provides 2018-8-6: We should assume they want all records for Aug 6, 2018
+     *
+     * Example 2: User provides 2018-8-6 10:30 : We should assume they want all records that occurred
+     * within that minute
+     *
+     * We can figure this out by going through each possible included time resolution, in order from
+     * smallest to biggest, and seeing if the value of it is 0. The first non-zero value we encounter
+     * will be the time resolution provided by the User.
+     */
+    const possibleResolutions = ['millisecond', 'second', 'minute', 'hour', 'date'];
+
+    for (let i = 0; i < possibleResolutions.length - 1; i++) {
+        const resolution = possibleResolutions[i];
+
+        // The Moment object has a function for each of these possible resolutions that returns a Number
+        if (date[resolution]() === 0) {
+            // If it is 0, then the requested time resolution must be at least the next step up
+            actualResolution = possibleResolutions[i + 1];
+        } else {
+            // If it is not 0, we have reached the time resolution provided by the User
+            break;
+        }
+    }
+
+    // We only accept timestamps with second-resolution, so this conditional should always resolve true
+    if (actualResolution) {
+        return {
+            value: {
+                $gte: date.startOf(actualResolution).toDate(),
+                $lte: date.endOf(actualResolution).toDate()
+            }
+        };
+    } else {
+        return { error: `An unknown error occurred parsing this date: ${value}` };
+    }
+}
+
+module.exports = {
+    DATE_FORMATS,
+    coerceValue,
+    coerceBoolean,
+    coerceNumber,
+    coerceObjectId,
+    coerceDate
+};
