5f49d33381211bc2130270dc5a3f10edf3b92aa1
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-08-16 16:00:35 -0500

    improve common tests

--- server/tests/utils/common-tests.js
+++ server/tests/utils/common-tests.js
@@ -4,11 +4,12 @@ const { expect } = chai;
 
 describe('Common Utilities', () => {
     describe('mapByName', () => {
-        it('Takes an array of Objects from the aws-sdk, and maps them by Name, Hostname, and Shortname', () => {
+        it('Takes an array of Objects from the aws-sdk, and maps them by lowercase Name, Hostname, and Shortname', () => {
             const testArray = [
                 { Name: 'long-test1' },
                 { Hostname: 'test2' },
-                { Shortname: 'test3' }
+                { Shortname: 'test3' },
+                { Name: 'TEST4' }
             ];
 
             const map = mapByName(testArray);
@@ -21,6 +22,9 @@ describe('Common Utilities', () => {
 
             expect(map).to.haveOwnProperty('test3');
             expect(map['test3'].Shortname).to.equal('test3');
+
+            expect(map).to.haveOwnProperty('test4');
+            expect(map['test4'].Name).to.equal('TEST4');
         });
     });
 });
